ba7ed2d89bdb6e64acc71acf131cb7b68dbe47a1
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2023-07-27 18:07:23 +0300

    initial files with first markup and simple styles for table with notes

---
+++ src/js/notes.js
@@ -0,0 +1,8 @@
+
+const list = document.getElementById("list");
+const listItemName = document.getElementById("list-name");
+const listItemCategory = document.getElementById("list-category");
+const listItemContent = document.getElementById("list-content");
+const listItemDate = document.getElementById("list-date");
+
+const createButton = document.querySelector("btn-create");
